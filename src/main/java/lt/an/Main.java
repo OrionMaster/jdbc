916b34fc448c4f9032e43df8b1f8cc7404a85d97
OrionMaster <masterrf@gmail.com>
2022-07-28 19:31:45 +0300

    refactoring metods

--- src/main/java/lt/an/Main.java
+++ src/main/java/lt/an/Main.java
@@ -3,20 +3,65 @@ package lt.an;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
 
-        Properties props = new Properties();
+        try (Connection con = getConnection()) {
+            ArrayList<Customer> customers = getCustomers(con);
+
+            customers.forEach(System.out::println);
+
+            } catch (Exception e){
+                System.out.println(e);
+            }
+        }
+
+    private static ArrayList<Customer> getCustomers(Connection con) throws SQLException {
+
+        Statement stmt = con.createStatement();
+
+        ResultSet rs = stmt.executeQuery("select * from customers");
+
+        ArrayList<Customer> customers = new ArrayList<>();
+
+        while (rs.next()) {
+            int customerNumber = rs.getInt("customerNumber");
+            String customerName = rs.getString("customerName");
+            String customerCity = rs.getString("city");
+            String customerPhone = rs.getString("phone");
+
+            Customer customer = new Customer( customerNumber, customerName, customerPhone, customerCity);
+            customers.add(customer);
+
+            }
+        return customers;
+    }
+
+    private static Connection getConnection() {
+        Properties props = getProperties();
+
+        String url = props.getProperty("db.url");
+        String username = props.getProperty("db.username");
+        String password = props.getProperty("db.password");
+
+        Connection con = null;
 
         try {
-            FileInputStream fileInputStream = new FileInputStream("db.properties");
+            con = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
+
+    private static Properties getProperties() {
+        Properties props = new Properties();
+
+        try (FileInputStream fileInputStream = new FileInputStream("db.properties");){
             props.load(fileInputStream);
 
         } catch (FileNotFoundException e) {
@@ -24,37 +69,8 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException();
         }
-
-        String url = props.getProperty("db.url");
-        String username = props.getProperty("db.username");
-        String password = props.getProperty("db.password");
-
-        try {
-            Connection con = DriverManager.getConnection(url, username, password);
-            Statement stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery("select * from customers");
-
-            ArrayList<Customer> customers = new ArrayList<>();
-
-            while (rs.next()) {
-                int customerNumber = rs.getInt("customerNumber");
-                String customerName = rs.getString("customerName");
-                String customerCity = rs.getString("city");
-                String customerPhone = rs.getString("phone");
-
-                Customer customer = new Customer( customerNumber, customerName, customerPhone, customerCity);
-                customers.add(customer);
-
-                }
-
-            con.close();
-            customers.forEach(System.out::println);
-
-            } catch (Exception e){
-                System.out.println(e);
-            }
-        }
+        return props;
     }
+}
 
 
